835bfc2cff23b0fc977709210a4ea1f28abf0ed4
Matt Bechtel <bechma02@gmail.com>
2019-10-18 20:01:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,7 @@
 first_number = 1
-second_number = 2
+second_number = 2.5
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
